f2cd0bb74f958491dd7078372b04f79c4f65ce23
oborogri <grigore.oboroceanu@gmail.com>
2016-11-29 11:57:41 +0000

    Donation-client(v_02): Async-http-client added

--- src/services/fixtures.js
+++ src/services/fixtures.js
@@ -1,45 +1,5 @@
 export default class Fixtures {
 
+  baseUrl = 'http://localhost:4000';
   methods = ['Cash', 'PayPal'];
-
-  candidates = [
-    {
-      firstName: 'Lisa',
-      lastName: 'Simpson',
-      office: 'New York'
-    },
-    {
-      firstName: 'Bart',
-      lastName: 'Simpson',
-      office: 'Chicago'
-    }
-  ];
-
-  users = {
-    'homer@simpson.com': {
-      firstName: 'Homer',
-      lastName: 'Simpson',
-      email: 'homer@simpson.com',
-      password: 'secret'
-    },
-    'marge@simpson.com': {
-      firstName: 'Marge',
-      lastName: 'Simpson',
-      email: 'marge@simpson.com',
-      password: 'secret'
-    }
-  }
-
-  donations = [
-    {
-      amount: 23,
-      method: 'cash',
-      candidate: this.candidates[0]
-    },
-    {
-      amount: 212,
-      method: 'paypal',
-      candidate: this.candidates[1]
-    }
-  ];
 }
